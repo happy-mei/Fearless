c23db512e27f0f33665e638fd3696ab7ba5f0dc8
Nick Webster <nick@nick.geek.nz>
2022-11-03 22:12:24 +1300

    We can use orElseThrow here

--- src/utils/OneOr.java
+++ src/utils/OneOr.java
@@ -6,6 +6,6 @@ public class OneOr{
   public static <T> T of(String err, Stream<T> ts){
     return ts
       .reduce( (a,b)->{throw new RuntimeException(err);})
-      .orElseGet(()->{throw new RuntimeException(err);});
+      .orElseThrow(()-> new RuntimeException(err));
   }
 }
