69440690821543dd2bf665adcfc4ad16e7b52cf3
Nick Webster <nick@nick.geek.nz>
2024-03-17 02:16:27 +1300

    Working on the block optimisation for .var

--- src/codegen/optimisations/BlockOptimisation.java
+++ src/codegen/optimisations/BlockOptimisation.java
@@ -29,20 +29,22 @@ public class BlockOptimisation implements MIRCloneVisitor {
     if (!isBlock) {
       return MIRCloneVisitor.super.visitFun(fun);
     }
-    return this.visitBlockCall(call).map(fun::withBody).orElse(MIRCloneVisitor.super.visitFun(fun));
+    return this.visitBlockCall(call, List.of(MIR.Block.BlockStmt.Return.class), Optional.empty())
+      .map(fun::withBody)
+      .orElse(MIRCloneVisitor.super.visitFun(fun));
   }
 
-  private Optional<MIR.E> visitBlockCall(MIR.MCall call) {
+  private Optional<MIR.E> visitBlockCall(MIR.MCall call, List<Class<? extends MIR.Block.BlockStmt>> validEndings, Optional<MIR.X> self) {
     var stmts = new ArrayDeque<MIR.Block.BlockStmt>();
-    var res = flattenBlock(call, stmts);
+    var res = flattenBlock(call, stmts, self);
     if (res == FlattenStatus.INVALID) { return Optional.empty(); }
-    if (!(stmts.getLast() instanceof MIR.Block.BlockStmt.Return)) { return Optional.empty(); }
+    if (validEndings.stream().noneMatch(c->c.isAssignableFrom(stmts.getLast().getClass()))) { return Optional.empty(); }
     assert res == FlattenStatus.FLATTENED;
     return Optional.of(new MIR.Block(call, Collections.unmodifiableCollection(stmts), call.t()));
   }
 
   private enum FlattenStatus { CONTINUE, INVALID, FLATTENED }
-  private FlattenStatus flattenBlock(MIR.E expr, Deque<MIR.Block.BlockStmt> stmts) {
+  private FlattenStatus flattenBlock(MIR.E expr, Deque<MIR.Block.BlockStmt> stmts, Optional<MIR.X> self) {
     return switch (expr) {
       case MIR.MCall mCall -> {
         if (mCall.name().equals(new Id.MethName("#", 0)) && this.magic.isMagic(Magic.BlockK, mCall.recv())) {
@@ -71,6 +73,16 @@ public class BlockOptimisation implements MIRCloneVisitor {
             yield FlattenStatus.INVALID;
           }
           stmts.offerFirst(new MIR.Block.BlockStmt.If(res.get()));
+        } else if (mCall.name().equals(new Id.MethName(".var", 2))) {
+          var variable = this.visitReturn(mCall.args().getFirst());
+          var continuationCall = this.visitVarContinuation(mCall.args().get(1));
+          if (variable.isEmpty() || continuationCall.isEmpty()) { yield FlattenStatus.INVALID; }
+          var continuation = this.visitBlockCall(
+            continuationCall.get().continuationCall(),
+            List.of(MIR.Block.BlockStmt.Return.class, MIR.Block.BlockStmt.Do.class), // TODO: and Error when we have that
+            Optional.of(continuationCall.get().selfVar())
+          );
+          throw Bug.todo();
         } else {
           // TODO: .var
           // TODO: .error
@@ -78,11 +90,11 @@ public class BlockOptimisation implements MIRCloneVisitor {
           // TODO: .varIso
           yield FlattenStatus.INVALID;
         }
-        yield flattenBlock(mCall.recv(), stmts);
+        yield flattenBlock(mCall.recv(), stmts, self);
       }
       case MIR.BoolExpr boolExpr -> throw Bug.todo();
       case MIR.CreateObj ignored -> FlattenStatus.INVALID;
-      case MIR.X ignored -> FlattenStatus.INVALID;
+      case MIR.X x -> self.filter(x::equals).map(x_->FlattenStatus.FLATTENED).orElse(FlattenStatus.INVALID);
       case MIR.Block ignored -> throw Bug.unreachable();
     };
   }
@@ -95,5 +107,15 @@ public class BlockOptimisation implements MIRCloneVisitor {
     assert m.sig().name().equals(new Id.MethName("#", 0));
     var body = this.funs.get(m.fName().orElseThrow()).body();
     return Optional.of(body);
+  }
+  private record VarContinuation(MIR.X selfVar, MIR.MCall continuationCall) {}
+  private Optional<VarContinuation> visitVarContinuation(MIR.E fn) {
+    if (!(fn instanceof MIR.CreateObj k)) { return Optional.empty(); }
+    if (!this.magic.isMagic(Magic.VarContinuation, k)) { return Optional.empty(); }
+    if (k.meths().size() != 1) { return Optional.empty(); }
+    var m = k.meths().getFirst();
+    assert m.sig().name().equals(new Id.MethName("#", 2));
+    var body = (MIR.MCall) this.funs.get(m.fName().orElseThrow()).body();
+    return Optional.of(new VarContinuation(m.sig().xs().get(1), body));
   }
 }
