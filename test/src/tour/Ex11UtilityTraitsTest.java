ed541b03b326f141af3291452d78c63ee90092c4
Nick Webster <nick@nick.geek.nz>
2024-03-18 00:20:05 +1300

    Fixing UtilityTraitsTest

--- test/src/tour/Ex11UtilityTraitsTest.java
+++ test/src/tour/Ex11UtilityTraitsTest.java
@@ -7,7 +7,7 @@ import utils.RunOutput;
 import static codegen.java.RunJavaProgramTests.ok;
 
 public class Ex11UtilityTraitsTest {
-  @Test void flowSumStr() { ok(new RunOutput.Res("30", "", 0), "test.Test", """
+  @Test void flowSumStr() { ok(new RunOutput.Res("", "", 0), "test.Test", """
     package test
     Test:Main {sys -> Block#
       .var[mut Person] personMut = {FPerson#("Bob")}
@@ -18,10 +18,10 @@ public class Ex11UtilityTraitsTest {
       .return{{}}
       }
     Foo:{ #(f: read F[read Person]): read Person -> f# }
-    FPerson:{ #(name: Str): Person -> Block#
+    FPerson:{ #(name: Str): mut Person -> Block#
       .var nameRef = {Ref#name}
-      .return {Person:{
-        read .name: Str -> nameRef.get,
+      .return {mut Person:{
+        read .name: Str -> nameRef.get.toImm,
         mut .name(newName: Str): Void -> nameRef.set(newName),
         }}
       }
