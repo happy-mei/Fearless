9cb3710da1ea2a6c013ef9b08edf9805d3ee96c8
Nick Webster <nick@nick.geek.nz>
2024-03-22 12:19:34 +1300

    Removing unused deps

--- src/module-info.java
+++ src/module-info.java
@@ -3,11 +3,7 @@ module FearlessCompiler {//TODO: can I use suppress warnings here?
   requires org.antlr.antlr4.runtime;
   requires org.junit.jupiter.api;
   requires org.opentest4j;
-  requires com.fasterxml.jackson.core;
-  requires com.fasterxml.jackson.annotation;
-  requires com.fasterxml.jackson.databind;
   requires java.compiler;
-//  requires org.graalvm.truffle;
   requires commons.cli;
   requires cmdline.app;
   requires faux.pas;
