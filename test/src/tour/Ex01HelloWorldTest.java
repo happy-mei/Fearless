24318861014abe96de5b735b9f49da24fa6f6727
Marco Servetto <marco.servetto@vuw.ac.nz>
2024-03-23 13:40:46 +1300

    Started writing the guide focused on types in TextDrafts.txt

--- test/src/tour/Ex01HelloWorldTest.java
+++ test/src/tour/Ex01HelloWorldTest.java
@@ -6,7 +6,8 @@ import org.junit.jupiter.api.Test;
 import static tour.TourHelper.*;
 public class Ex01HelloWorldTest {
 /*
-----A tour of Fearless via the standard library----
+# A tour of Fearless via the standard library
+
 A Fearless project is a folder containing files with extension *.fear.
 Those files can be at top level or inside folders.
 The organization of files in folders is for the benefit of the programmers and
@@ -70,10 +71,15 @@ Here we show how to read and write files.
 We use `io` to open a  `mut File file`.
 Note how we use `[..]` to specify the type of the local variable. The type can often be inferred,
 we write it down here explicitly for clarity.
-'file.read' reads the content of the file, but it does not directly return a string, since the operation
-may fail. Instead, it returns an `Either[Str]` type.
-The method `!` of either gets the string out (in the positive case), or throws an error in the case the
-file could not be read.
+'file.read' does not directly read the content of the file,
+but it creates an Action[Str] object, that it does.
+The method `!` of Action gets the string out (in the positive case),
+or throws an error in the case the file could not be read.
+A programmer expert in Java or python would have probably expected
+file.read to either return the string or throw the error.
+Returning an Action object allows to provide an uniform API
+to handle operations that can fail in recoverable ways,
+that is, where the failure is not an observed bug.
 -------------------------*/@Disabled /*TODO: unimplemented lib code*/ @Test void fsWriteHello() { run("""
     Test:Main {sys -> Block#
       .var   io = {FIO#sys}
@@ -83,12 +89,14 @@ file could not be read.
       }
     """); }/*--------------------------------------------
 Consider this other example, writing on the file.
-`file.write` does not throw an error if the operation fails, but returns an `Either[Void]`.
-In most languages this would be bad API design, since the error could be ignored if the `Either[Void]`
-is simply discarded. This concern is less valid in Fearless, where discarding values by accident is unlikely.
+`file.write` does not throw an error if the operation fails, but returns an `Action[Void]`.
+That is, until we call the '!' method the file is not being written.
+Now, In most languages this would be bad API design, since the action could be ignored instead of
+being performed, if the `Action[Void]` object is simply discarded.
+This concern is less valid in Fearless, where discarding values by accident is unlikely.
 If the user forgot to use the `!` and just wrote
 `.return {file.write(c)}`, the code would have not compiled.
-To return for that block, we need a `Void` value, but `file.write(c)` produces an `Either[Void]`.
+To return for that block, we need a `Void` value, but `file.write(c)` produces an `Action[Void]`.
 
 Overall, `IO` handles file system interactions (local IO) while Network IO is handled by another capability.
 */
