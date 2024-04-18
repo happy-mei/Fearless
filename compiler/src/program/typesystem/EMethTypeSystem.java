5fd0684925060eebcadcac0bd39416604ac6f43e
marcoServeto <marco.servetto@gmail.com>
2024-04-19 11:33:11 +1200

    Changes by nick

--- compiler/src/program/typesystem/EMethTypeSystem.java
+++ compiler/src/program/typesystem/EMethTypeSystem.java
@@ -58,8 +58,9 @@ public interface EMethTypeSystem extends ETypeSystem {
     List<E> es = Push.of(e.receiver(),e.es());
     var nestedErrors = new ArrayDeque<ArrayList<Supplier<CompileError>>>(tsts.size());
     var methArgCache = IntStream.range(0, es.size()).mapToObj(i_->new HashMap<T, Res>()).toList();
+    var guessType = new GuessITX(p(), g(), xbs(), depth());
     for (var tst : tsts) {
-      var errors = new ArrayList<Supplier<? extends CompileError>>();
+      var errors = new ArrayList<Supplier<CompileError>>();
       nestedErrors.add(errors);
       if (okAll(es, tst.ts(), errors, methArgCache, guessType)) {
         //note: the receiver can be a subtype of the declaration point like all the other
--- compiler/src/program/typesystem/GuessIT.java
+++ compiler/src/program/typesystem/GuessIT.java
@@ -6,26 +6,27 @@ import id.Id;
 import id.Mdf;
 import program.Program;
 import program.TypeRename;
+import utils.Bug;
+import utils.OneOr;
 import visitors.Visitor;
 
 import java.util.Set;
 import java.util.stream.Collectors;
 
-public record GuessIT(Program p, Gamma g, XBs xbs, int depth) implements Visitor<Set<Id.IT<T>>> {
-  @Override public Set<Id.IT<T>> visitX(E.X e) {
-    return g().get(e).match(gx->Set.of(), Set::of);
+public record GuessIT(Program p, Gamma g, XBs xbs, int depth) implements Visitor<Id.IT<T>> {
+  @Override public Id.IT<T> visitX(E.X e) {
+    return g().get(e).itOrThrow();
   }
-  @Override public Set<Id.IT<T>> visitLambda(E.Lambda e) {
-    return Set.copyOf(e.its());
+  @Override public Id.IT<T> visitLambda(E.Lambda e) {
+    return e.name().toIT();
   }
-  @Override public Set<Id.IT<T>> visitMCall(E.MCall e) {
+  @Override public Id.IT<T> visitMCall(E.MCall e) {
     var renamer = TypeRename.core(p());
-    return e.receiver().accept(this).stream()
-      .flatMap(recv->p().meths(xbs(), Mdf.recMdf, recv, depth()).stream())
+    var recv = e.receiver().accept(this);
+    return OneOr.of("", this.p().meths(this.xbs(), Mdf.recMdf, recv, this.depth()).stream()
       .filter(cm->cm.name().nameArityEq(e.name()))
-      .map(cm->renamer.renameSigOnMCall(cm.sig(), xbs().addBounds(cm.sig().gens(), cm.bounds()), renamer.renameFun(e.ts(), cm.sig().gens())))
+      .map(cm->renamer.renameSigOnMCall(cm.sig(), this.xbs().addBounds(cm.sig().gens(), cm.bounds()), renamer.renameFun(e.ts(), cm.sig().gens())))
       .filter(sig->sig.ret().isIt())
-      .map(sig->sig.ret().itOrThrow())
-      .collect(Collectors.toSet());
+      .map(sig->sig.ret().itOrThrow()));
   }
 }
