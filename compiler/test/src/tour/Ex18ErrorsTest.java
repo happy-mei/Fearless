fc30b373e35594b80426b4cca427abd756328658
Nick Webster <nick@nick.geek.nz>
2024-12-03 16:35:34 +1300

    Fixing Try/Catch tests

--- compiler/test/src/tour/Ex18ErrorsTest.java
+++ compiler/test/src/tour/Ex18ErrorsTest.java
@@ -7,25 +7,25 @@ import static utils.RunOutput.Res;
 import static codegen.java.RunJavaProgramTests.*;
 
 public class Ex18ErrorsTest {
-  @Test void uncaughtStackOverflow() { ok(new Res("", "Program crashed with: Stack overflowed", 1), """
+  @Test void uncaughtStackOverflow() { ok(new Res("", "Program crashed with: Stack overflowed[###]", 1), """
     package test
     Test:Main {sys -> Loop!}
     Loop: { ![R]: R -> this! }
     """, Base.mutBaseAliases); }
   @Test void caughtStackOverflow() { ok(new Res("", "Stack overflowed", 0), """
     package test
-    Test:Main {sys -> UnrestrictedIO#sys.printlnErr(CapTries#sys#[Void]{Loop!}.err!.msg)}
+    Test:Main {sys -> UnrestrictedIO#sys.printlnErr(CapTries#sys#[Void]{Loop!}.info!.msg)}
     Loop: { ![R]: R -> this! }
     """, Base.mutBaseAliases); }
   @Test void caughtDivByZero() { ok(new Res("", "/ by zero", 0), """
     package test
-    Test:Main {sys -> UnrestrictedIO#sys.printlnErr(Try#[Int]{+12 / +0}.err!.msg)}
+    Test:Main {sys -> UnrestrictedIO#sys.printlnErr(Try#[Int]{+12 / +0}.info!.msg)}
     """, Base.mutBaseAliases); }
   @Test void caughtFearlessError() { ok(new Res("", "\"oh no\"", 0), """
     package test
-    Test:Main {sys -> UnrestrictedIO#sys.printlnErr(Try#[Int]{Error.msg "oh no"}.err!.str)}
+    Test:Main {sys -> UnrestrictedIO#sys.printlnErr(Try#[Int]{Error.msg "oh no"}.info!.str)}
     """, Base.mutBaseAliases); }
-  @Test void uncaughtFearlessError() { ok(new Res("", "Program crashed with: \"oh no\"", 1), """
+  @Test void uncaughtFearlessError() { ok(new Res("", "Program crashed with: \"oh no\"[###]", 1), """
     package test
     Test:Main {sys -> Error.msg "oh no"}
     """, Base.mutBaseAliases); }
