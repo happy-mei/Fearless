a952a1aa3c0a15c4391df89c8977f034c5dcadd1
Nick Webster <nick@nick.geek.nz>
2025-04-29 14:36:05 +1200

    Defaulting to what works with maven

--- compiler/src/utils/LocalResourcesTemplate.java
+++ compiler/src/utils/LocalResourcesTemplate.java
@@ -4,8 +4,7 @@ import java.nio.file.Path;
 
 public class LocalResourcesTemplate {
   //example for windows
-  static public final Path compilerPath= Path.of("C:\\")
-    .resolve("Users","<userNameHere>","OneDrive","Documents","GitHub","Fearless","compiler");
+  static public final Path compilerPath= Path.of("");
   static public final String javaVersion= "23";
   }
   //example for linux
