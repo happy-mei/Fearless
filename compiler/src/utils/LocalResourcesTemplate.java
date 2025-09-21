204e034a7878eddec2b2f2029e67fa9203518b74
Nick Webster <nick@nick.geek.nz>
2025-09-21 16:06:03 +1200

    Java 25

--- compiler/src/utils/LocalResourcesTemplate.java
+++ compiler/src/utils/LocalResourcesTemplate.java
@@ -5,7 +5,7 @@ import java.nio.file.Path;
 public class LocalResourcesTemplate {
   //example for windows
   static public final Path compilerPath= Path.of("");
-  static public final String javaVersion= "24";
+  static public final String javaVersion= "25";
   }
   //example for linux
   //example for mac
